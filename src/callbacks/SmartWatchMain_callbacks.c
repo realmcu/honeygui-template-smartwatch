a67926b2b45e87966ab35503b00cadb534577656
luke_sun <luke_sun@realsil.com.cn>
2026-03-09 17:51:26 +0800

    feat(smartwatch): refactor time display to separate hours and minutes
    Change-Id: I56fd108cefb7197745ec8ab392700debf7c9f094

--- src/callbacks/SmartWatchMain_callbacks.c
+++ src/callbacks/SmartWatchMain_callbacks.c
@@ -5,7 +5,8 @@
 #include <time.h>
 
 // 时间字符串全局变量（在 UI 文件中定义）
-extern char hg_time_label_1768896234329_v8yi_time_str[10];
+extern char hg_time_label_hh_time_str[4];
+extern char hg_time_label_mm_time_str[4];
 extern char hg_time_label_1772765275313_pgx4_time_str[10];
 extern char hg_time_label_1772765275313_pgx4_copy_1772765661189_2_time_str[10];
 
@@ -20,42 +21,48 @@ void bottom_View_weather_switch_view_cb(void *obj, gui_event_t *e)
 {
     GUI_UNUSED(obj);
     GUI_UNUSED(e);
-    gui_view_switch_direct(gui_view_get_current(), "app_weatherMainView", SWITCH_OUT_ANIMATION_FADE, SWITCH_IN_ANIMATION_FADE);
+    gui_view_switch_direct(gui_view_get_current(), "app_weatherMainView", SWITCH_OUT_ANIMATION_FADE,
+                           SWITCH_IN_ANIMATION_FADE);
 }
 
 void hg_image_1769134788793_og20_switch_view_cb(void *obj, gui_event_t *e)
 {
     GUI_UNUSED(obj);
     GUI_UNUSED(e);
-    gui_view_switch_direct(gui_view_get_current(), "app_activity_view", SWITCH_OUT_ANIMATION_FADE, SWITCH_IN_ANIMATION_FADE);
+    gui_view_switch_direct(gui_view_get_current(), "app_activity_view", SWITCH_OUT_ANIMATION_FADE,
+                           SWITCH_IN_ANIMATION_FADE);
 }
 
 void hg_image_1768183941920_4wxc_switch_view_cb(void *obj, gui_event_t *e)
 {
     GUI_UNUSED(obj);
     GUI_UNUSED(e);
-    gui_view_switch_direct(gui_view_get_current(), "app_music_view", SWITCH_OUT_ANIMATION_FADE, SWITCH_IN_ANIMATION_FADE);
+    gui_view_switch_direct(gui_view_get_current(), "app_music_view", SWITCH_OUT_ANIMATION_FADE,
+                           SWITCH_IN_ANIMATION_FADE);
 }
 
 void hg_image_1768184009679_rg6a_switch_view_cb(void *obj, gui_event_t *e)
 {
     GUI_UNUSED(obj);
     GUI_UNUSED(e);
-    gui_view_switch_direct(gui_view_get_current(), "app_message_view", SWITCH_OUT_ANIMATION_FADE, SWITCH_IN_ANIMATION_FADE);
+    gui_view_switch_direct(gui_view_get_current(), "app_message_view", SWITCH_OUT_ANIMATION_FADE,
+                           SWITCH_IN_ANIMATION_FADE);
 }
 
 void hg_arc_1768184103087_n36y_switch_view_cb(void *obj, gui_event_t *e)
 {
     GUI_UNUSED(obj);
     GUI_UNUSED(e);
-    gui_view_switch_direct(gui_view_get_current(), "app_battery_view", SWITCH_OUT_TO_BOTTOM_USE_TRANSLATION, SWITCH_IN_ANIMATION_FADE);
+    gui_view_switch_direct(gui_view_get_current(), "app_battery_view",
+                           SWITCH_OUT_TO_BOTTOM_USE_TRANSLATION, SWITCH_IN_ANIMATION_FADE);
 }
 
 void bottom_View_tag_bg_menu_switch_view_cb(void *obj, gui_event_t *e)
 {
     GUI_UNUSED(obj);
     GUI_UNUSED(e);
-    gui_view_switch_direct(gui_view_get_current(), "app_menu_view", SWITCH_OUT_ANIMATION_FADE, SWITCH_IN_ANIMATION_FADE);
+    gui_view_switch_direct(gui_view_get_current(), "app_menu_view", SWITCH_OUT_ANIMATION_FADE,
+                           SWITCH_IN_ANIMATION_FADE);
 }
 
 void SmartWatchTemplateMainView_key_0_cb(void *obj, gui_event_t *e)
@@ -65,7 +72,8 @@ void SmartWatchTemplateMainView_key_0_cb(void *obj, gui_event_t *e)
     // 检查按键名
     if (strcmp(e->indev_name, "Menu") == 0)
     {
-        gui_view_switch_direct(gui_view_get_current(), "app_menu_view", SWITCH_INIT_STATE, SWITCH_IN_ANIMATION_FADE);
+        gui_view_switch_direct(gui_view_get_current(), "app_menu_view", SWITCH_INIT_STATE,
+                               SWITCH_IN_ANIMATION_FADE);
     }
 }
 
@@ -76,65 +84,79 @@ void samrtWatch_window_key_0_cb(void *obj, gui_event_t *e)
     // 检查按键名
     if (strcmp(e->indev_name, "Menu") == 0)
     {
-        gui_view_switch_direct(gui_view_get_current(), "app_menu_view", SWITCH_OUT_ANIMATION_FADE, SWITCH_IN_ANIMATION_FADE);
+        gui_view_switch_direct(gui_view_get_current(), "app_menu_view", SWITCH_OUT_ANIMATION_FADE,
+                               SWITCH_IN_ANIMATION_FADE);
     }
 }
 
-void hg_time_label_1768896234329_v8yi_time_update_cb(void *p)
+void hg_time_label_hh_time_update_cb(void *p)
 {
     GUI_UNUSED(p);
-    
+
     time_t now = time(NULL);
     struct tm *t = localtime(&now);
     if (t == NULL)
     {
         return;
     }
-    
-    // 更新时间字符串
-    sprintf(hg_time_label_1768896234329_v8yi_time_str, "%02d:%02d", t->tm_hour, t->tm_min);
-    
-    // 更新小时组件（前2个字符）
-    if (hg_time_label_1768896234329_v8yi_hour) {
-        gui_text_content_set(hg_time_label_1768896234329_v8yi_hour, hg_time_label_1768896234329_v8yi_time_str, 2);
+
+    sprintf(hg_time_label_hh_time_str, "%02d", t->tm_hour);
+
+    gui_text_content_set((gui_text_t *)hg_time_label_hh, hg_time_label_hh_time_str,
+                         strlen(hg_time_label_hh_time_str));
+}
+
+void hg_time_label_mm_time_update_cb(void *p)
+{
+    GUI_UNUSED(p);
+
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+    if (t == NULL)
+    {
+        return;
     }
-    
-    // 更新分钟组件（后2个字符，跳过冒号）
-    if (hg_time_label_1768896234329_v8yi_min) {
-        gui_text_content_set(hg_time_label_1768896234329_v8yi_min, hg_time_label_1768896234329_v8yi_time_str + 3, 2);
-    }
+
+    sprintf(hg_time_label_mm_time_str, "%02d", t->tm_min);
+
+    gui_text_content_set((gui_text_t *)hg_time_label_mm, hg_time_label_mm_time_str,
+                         strlen(hg_time_label_mm_time_str));
 }
 
 void hg_time_label_1772765275313_pgx4_time_update_cb(void *p)
 {
     GUI_UNUSED(p);
-    
+
     time_t now = time(NULL);
     struct tm *t = localtime(&now);
     if (t == NULL)
     {
         return;
     }
-    
+
     sprintf(hg_time_label_1772765275313_pgx4_time_str, "%02d:%02d", t->tm_hour, t->tm_min);
-    
-    gui_text_content_set((gui_text_t *)hg_time_label_1772765275313_pgx4, hg_time_label_1772765275313_pgx4_time_str, strlen(hg_time_label_1772765275313_pgx4_time_str));
+
+    gui_text_content_set((gui_text_t *)hg_time_label_1772765275313_pgx4,
+                         hg_time_label_1772765275313_pgx4_time_str, strlen(hg_time_label_1772765275313_pgx4_time_str));
 }
 
 void hg_time_label_1772765275313_pgx4_copy_1772765661189_2_time_update_cb(void *p)
 {
     GUI_UNUSED(p);
-    
+
     time_t now = time(NULL);
     struct tm *t = localtime(&now);
     if (t == NULL)
     {
         return;
     }
-    
-    sprintf(hg_time_label_1772765275313_pgx4_copy_1772765661189_2_time_str, "%02d:%02d", t->tm_hour, t->tm_min);
-    
-    gui_text_content_set((gui_text_t *)hg_time_label_1772765275313_pgx4_copy_1772765661189_2, hg_time_label_1772765275313_pgx4_copy_1772765661189_2_time_str, strlen(hg_time_label_1772765275313_pgx4_copy_1772765661189_2_time_str));
+
+    sprintf(hg_time_label_1772765275313_pgx4_copy_1772765661189_2_time_str, "%02d:%02d", t->tm_hour,
+            t->tm_min);
+
+    gui_text_content_set((gui_text_t *)hg_time_label_1772765275313_pgx4_copy_1772765661189_2,
+                         hg_time_label_1772765275313_pgx4_copy_1772765661189_2_time_str,
+                         strlen(hg_time_label_1772765275313_pgx4_copy_1772765661189_2_time_str));
 }
 
 /* @protected start custom_functions */
@@ -197,7 +219,7 @@ void win_clock_big_timer_0_cb(void *obj)
 {
     GUI_UNUSED(obj);
     GUI_UNUSED(text_date_big_timer_cnt);
-    
+
     if (list_card->offset < 93)
     {
         gui_obj_hidden((void *)win_clock_big, true);
